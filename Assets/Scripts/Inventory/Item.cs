8beab73aea082d7ba190f5fe24014c5ba06962d4
Maria Barraza <reikafer@gmail.com>
2020-03-31 15:43:13 -0700

    Consumable items

---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+   [SerializeField]
+    protected string objectName = "Potion";
+
+    [SerializeField,TextArea(3,10)]
+    protected string description;
+
+    [SerializeField]
+    Sprite icon;
+}
