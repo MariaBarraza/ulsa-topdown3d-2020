ff4e7ff79c1761ecc6b04edeaa309951ab21f4e6
Maria Barraza <reikafer@gmail.com>
2020-04-24 16:53:27 -0700

    enemy part 2

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField, Range(0.1f,10f)]
+    float moveSpeed;
+
+    [SerializeField, Range(0f,10f)]
+    float minDistance;
+
+    NavMeshAgent navMeshAgent;
+
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if(Attack)
+        {
+            navMeshAgent.destination = GameManager.instance.Player.transform.position;
+            transform.LookAt(GameManager.instance.Player.transform);
+        }   
+    }
+
+    public bool Attack
+    {
+        get => Vector3.Distance(this.transform.position,GameManager.instance.Player.transform.position) <= minDistance;
+    }
+}
---
+++ Assets/Scripts/Inventory/DialogTextBox.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogTextBox : MonoBehaviour
+{
+    [SerializeField]
+    Text dialog;
+
+    [SerializeField]
+    string message;
+
+    IEnumerator animateText;
+
+    [SerializeField, Range(0.01f, 4f)]
+    float animationsSpeed;
+
+    /*public string TextDialog
+    {
+        set => dialog.text = value;
+    }*/
+
+    public void ClearText()
+    {
+        dialog.text = "";
+    }
+    public void ShowDialog()
+    {
+        animateText = Animate(animationsSpeed);
+        StartCoroutine(animateText);
+    }
+   
+
+    public string Message { get => message; set => message = value; }
+
+    IEnumerator Animate(float time)
+    {
+     int i = 0;
+        while(i < Message.Length)
+        {
+            dialog.text += message[i];
+            i++;
+            yield return new WaitForSeconds(time); 
+        }
+    }
+}
